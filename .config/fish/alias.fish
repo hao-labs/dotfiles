65739ac01f18cd1fa7035398f5d3e804de3e1da2
Cahyo Bagus L <hao.labs.2011@gmail.com>
2022-01-26 17:08:33 +0700

    feat(alias): update alias for git diff and branch

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -12,14 +12,15 @@ abbr -a gs  git status -sb
 abbr -a ga  git add
 abbr -a gc  git commit
 # switch branch
-abbr -a gco git checkout
+abbr -a gco git-checkout
+abbr -a gb  git branch -av
 # interract with remote
 abbr -a gcl git clone
 abbr -a gp  git push
 abbr -a gpl git pull
 # compare file
-abbr -a gd  git diff
-abbr -a gds git diff --staged
+abbr -a gd  git difftool
+abbr -a gds git difftool --staged
 # log commit changes
 abbr -a gl  git log-default
 abbr -a glc git log-compact
@@ -30,6 +31,8 @@ abbr -a fab git find-all-branch-related-to-commit
 # undo commit
 abbr -a gu  git undo-last-commit
 abbr -a gw  git wipe-last-commit
+# merge
+abbr -a gm  git merge
 
 # exa
 abbr -a l  list
