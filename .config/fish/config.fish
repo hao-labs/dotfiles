755c6768c33b595152ab3fec350266c096fe9886
Cahyo Bagus L <hao.labs.2011@gmail.com>
2024-03-03 16:23:26 +0700

    feat(fish): add auto completion from brew vendor
    brew vendor means installed app through brew that have fish auto completion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,4 +15,13 @@ if status is-interactive
   # pnpm end
 end
 
+# Auto Completions for Fish shell
 git-town completions fish | source
+
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
